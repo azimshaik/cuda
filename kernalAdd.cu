d9b700111ea3801f1d1873a9de71999be274016f
azimshaik <cheer.buddu@gmail.com>
2016-10-20 16:26:16 -0400

    Tried to understand thread and block ids :(

--- kernalAdd.cu
+++ kernalAdd.cu
@@ -1,19 +1,24 @@
 #include <iostream>
 #include "book.h"
 #include <stdio.h>
-__global__ void add(int a, int b, int *c){
-	int tid = blockIdx.x +threadIdx.x;
-	*c = tid;
+__global__ void add(int a, int b, int *c, int *d){
+	int bid = threadIdx.x;
+	int tid = threadIdx.y;
+	*c = bid;
+	*d = tid;
 }
 
 int main(void){
-	int c;
-	int *dev_c;
+	int c,d;
+	int *dev_c, *dev_d;
 	cudaMalloc( (void**)&dev_c, sizeof(int) );
-	
-	add<<<1,10>>>(2,7,dev_c) ;
-	HANDLE_ERROR(  cudaMemcpy( &c, dev_c, sizeof(int), cudaMemcpyDeviceToHost));
-	printf("2+7= %d\n",c);
+	cudaMalloc( (void**)&dev_d, sizeof(int) );	
+	add<<<5,100>>>(2,7,dev_c, dev_d) ;
+	cudaMemcpy( &c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy( &d, dev_d, sizeof(int), cudaMemcpyDeviceToHost);
+	printf("this is threadIdx.x : %d\n",c);
+	printf("this is threadIdx.x:  %d\n",d);
 	cudaFree(dev_c);
+	cudaFree(dev_d);
 	return 0; 
 }
