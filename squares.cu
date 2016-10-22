69bce36b476876770a030283e96ab674ff34741f
azimshaik <cheer.buddu@gmail.com>
2016-10-22 14:38:45 -0400

    Pointers Reference and deference

---
+++ squares.cu
@@ -0,0 +1,14 @@
+//Program for squaring numbers in  an array on NVIOIA device
+#include <stdio.h>
+#include "book.h"
+
+int  main (){
+	int x = 1, y=2;
+	int* a,* b;
+	a = &x;
+	b = &y;
+	printf("a has reference to  %d\n",*a);
+	
+
+}
+
