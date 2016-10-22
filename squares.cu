2c91eeb4994e18d6645134e77f8a79adeb01e2d0
azimshaik <cheer.buddu@gmail.com>
2016-10-22 15:28:39 -0400

    Block size and numbers

--- squares.cu
+++ squares.cu
@@ -3,12 +3,23 @@
 #include "book.h"
 
 int  main (){
-	int x = 1, y=2;
-	int* a,* b;
-	a = &x;
-	b = &y;
-	printf("a has reference to  %d\n",*a);
 	
+	int* array_host,* array_device;
+	int N = 10;
+	array_host = (int*)malloc(N*sizeof(int));
+	cudaMalloc((void**)&array_device, N*sizeof(int));
+	int i;
+	for(i=0;i<N;i++)
+	{
+		array_host[i] = i;
+		printf("array_host:%d \n ",array_host[i]);
+	}
+	int size = N;
+	cudaMemcpy(array_device, array_host,size,cudaMemcpyHostToDevice);
+	//block size and dimentions
+	int blocksize = 4;
+	int num_of_blocks = N/blocksize + (N%blocksize == 0 ? 0:1);
+	//Kernell(deice call)
 
 }
 
